14b62b9f8767214aaabb96b08795e2eef46ba943
James Halliday <substack@gmail.com>
2011-02-05 19:36:00 -0800

    splice implementation with failing test

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
-module.exports = function () {
-    var offset = 0;
-    var buffers = [];
+module.exports = function Buffers (bufs) {
+    var buffers = bufs || [];
     
     var self = { length : 0 };
-    
-    self.advance = function (n) {
-        offset += n;
-        while (buffers.length > 0 && offset > buffers[0].length) {
-            offset -= buffers[0].length;
-            self.length -= buffers[0].length;
-            buffers.shift();
-        }
-    };
     
     self.push = function (buf) {
         buffers.push(buf);
         self.length += buf.length;
     };
     
+    self.splice = function (i, howMany) {
+        var index = i >= 0 ? i : self.length - i;
+        var xs = [].slice.call(arguments, 2);
+        
+        if (howMany === undefined) {
+            howMany = self.length - index;
+        }
+        
+        var removed = new Buffers();
+        
+        var startBytes = 0;
+        for (
+            var si = 0;
+            si < buffers.length && startBytes + buffers[i].length <= index;
+            si ++
+        ) { startBytes += buffers[si].length }
+        
+        var rmBytes = 0;
+        for (
+            var ei = 0;
+            ei < buffers.length && rmBytes + buffers[i].length <= howMany;
+            ei ++
+        ) {
+            if (rmBytes === 0) {
+                removed.push(buffers[ei].slice(index - startBytes));
+            }
+            else {
+                removed.push(buffers[ei]);
+            }
+            rmBytes += buffers[ei].length;
+        }
+        
+        if (rmBytes < howMany) {
+            removed.push(
+                buffers[ei].slice(0, howMany - rmBytes)
+            );
+        }
+        
+        buffers.splice(si, ei, xs);
+        self.length -= rmBytes - xs.reduce(function (x) { return x.length }, 0);
+        
+        return removed;
+    };
+    
     self.slice = function (i, j) {
         if (j === undefined) j = self.length;
         if (i === undefined) i = 0;
-        i += offset; j += offset;
         
         if (j > self.length) {
             throw new Error('Index ' + j + ' out of bounds');
@@ -31,7 +64,7 @@ module.exports = function () {
         for (
             var si = 0;
             si < buffers.length && startBytes + buffers[si].length <= i;
-            si++
+            si ++
         ) { startBytes += buffers[si].length }
         
         var target = new Buffer(j - i);
--- test/buffers.js
+++ test/buffers.js
@@ -22,3 +22,42 @@ exports.slice = function () {
     }
 };
 
+exports.splice = function () {
+    var xs = [0,1,2,3,4,5,6,7,8,9];
+    var splits = [ [4,2,3,1], [2,2,2,2,2], [1,6,3,1], [9,2], [10], [5,5] ];
+    
+    splits.forEach(function (split) {
+        function create () {
+            var bufs = Buffers();
+            var offset = 0;
+            split.forEach(function (i) {
+                bufs.push(new Buffer(xs.slice(offset, offset + i)));
+                offset += i;
+            });
+            return bufs;
+        }
+        
+        for (var i = 0; i < xs.length; i++) {
+            for (var j = i; j < xs.length; j++) {
+                var bufs = create();
+                var xs_ = xs.slice();
+                
+                var a = bufs.splice(i,j);
+                var b = new Buffers(new Buffer(xs_.splice(i,j)));
+                
+                assert.eql(a.slice(), b.slice(),
+                    '[' + [].join.call(a, ',') + ']'
+                        + ' != ' + 
+                    '[' + [].join.call(b, ',') + ']'
+                );
+                
+                assert.eql(
+                    '[' + [].join.call(bufs.slice(), ',') + ']'
+                        + ' != ' + 
+                    '[' + [].join.call(xs_, ',') + ']'
+                );
+            }
+        }
+    });
+};
+
