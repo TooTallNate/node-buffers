5d02099fce966c9060a47b99f36e2b20e169a34e
James Halliday <substack@gmail.com>
2011-02-06 01:23:28 -0800

    using splits for slice test

--- test/buffers.js
+++ test/buffers.js
@@ -1,32 +1,41 @@
 var assert = require('assert');
 var Buffers = require('buffers');
 
-exports.slice = function () {
+function create (xs, split) {
     var bufs = Buffers();
-    bufs.push(new Buffer([0,1,2,3]));
-    bufs.push(new Buffer([4,5]));
-    bufs.push(new Buffer([6,7,8]));
-    bufs.push(new Buffer([9]));
-    
+    var offset = 0;
+    split.forEach(function (i) {
+        bufs.push(new Buffer(xs.slice(offset, offset + i)));
+        offset += i;
+    });
+    return bufs;
+}
+
+exports.slice = function () {
     var xs = [0,1,2,3,4,5,6,7,8,9];
-    assert.eql(new Buffer(xs), bufs.slice(),
-        '[' + xs.join(',') + ']'
-            + ' != ' + 
-        '[' + [].join.call(bufs.slice(), ',') + ']'
-    );
+    var splits = [ [4,2,3,1], [2,2,2,2,2], [1,6,3,1], [9,2], [10], [5,5] ];
     
-    for (var i = 0; i < xs.length; i++) {
-        for (var j = i; j < xs.length; j++) {
-            var a = bufs.slice(i,j);
-            var b = new Buffer(xs.slice(i,j));
-            
-            assert.eql(a, b,
-                '[' + [].join.call(a, ',') + ']'
-                    + ' != ' + 
-                '[' + [].join.call(b, ',') + ']'
-            );
+    splits.forEach(function (split) {
+        var bufs = create(xs, split);
+        assert.eql(new Buffer(xs), bufs.slice(),
+            '[' + xs.join(',') + ']'
+                + ' != ' + 
+            '[' + [].join.call(bufs.slice(), ',') + ']'
+        );
+        
+        for (var i = 0; i < xs.length; i++) {
+            for (var j = i; j < xs.length; j++) {
+                var a = bufs.slice(i,j);
+                var b = new Buffer(xs.slice(i,j));
+                
+                assert.eql(a, b,
+                    '[' + [].join.call(a, ',') + ']'
+                        + ' != ' + 
+                    '[' + [].join.call(b, ',') + ']'
+                );
+            }
         }
-    }
+    });
 };
 
 exports.splice = function () {
@@ -34,19 +43,9 @@ exports.splice = function () {
     var splits = [ [4,2,3,1], [2,2,2,2,2], [1,6,3,1], [9,2], [10], [5,5] ];
     
     splits.forEach(function (split) {
-        function create () {
-            var bufs = Buffers();
-            var offset = 0;
-            split.forEach(function (i) {
-                bufs.push(new Buffer(xs.slice(offset, offset + i)));
-                offset += i;
-            });
-            return bufs;
-        }
-        
         for (var i = 0; i < xs.length; i++) {
             for (var j = i; j < xs.length; j++) {
-                var bufs = create();
+                var bufs = create(xs, split);
                 var xs_ = xs.slice();
                 
                 var a = bufs.splice(i,j);
